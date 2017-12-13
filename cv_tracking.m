a11a46280bbbfb83f815120ac2274a5487e77c54
BrianAmin <tuf49787@temple.edu>
2017-12-12 20:09:29 -0500

    Initial push

---
+++ cv_tracking.m
@@ -0,0 +1,50 @@
+% Load in video
+
+video_filename = uigetfile('*.*')
+
+if ( length(video_filename) > 0)
+    vid_read_obj = VideoReader(video_filename)
+    
+else
+    disp('File not loaded')
+    
+end
+
+% Find region of interest
+frame = readFrame(vid_read_obj);
+frame = flipdim(frame ,2);
+frame = flipdim(frame ,1);
+fig = figure;
+imshow(frame)
+[x,y] = getpts(fig)
+%h = msgbox('Specify Points to Crop out and hit ENTER')
+
+
+
+while hasFrame(vid_read_obj)
+    
+    frame = readFrame(vid_read_obj);
+    frame = flipdim(frame,2);
+    frame = flipdim(frame,1);
+    crop = imcrop(frame,[x(1),y(1),x(2)-x(1),y(2)-y(1)]);
+    crop_gry = rgb2gray(crop);
+    crop_guass = imgaussfilt(crop_gry, 10.0);
+    crop_bw = edge(crop_guass,'canny');
+    %imshow(crop_bw)
+    contours = imcontour(crop_bw);
+
+    
+    
+    
+end
+
+
+
+% While loop to go over each frame for the length of the video
+
+
+
+% In loop, you will crop the frame to ROI, and then threshold out the
+% markers
+
+% Crunch angles and save to a matrix [frame_number joint_angle]
